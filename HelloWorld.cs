c14bda11735dcc93944286e54f80468dfd69d7a4
Tamermass2024 <masarwe.tamer5@gmail.com>
2024-12-07 10:23:03 +0200

    This file is our firts one

---
+++ HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print Hi and 2024
+        print(2024);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+}
+
